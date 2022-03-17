954c19fd015f1af00270b9f37a7e98f7502e8ec7
Kevin Hou <neonnova916@gmail.com>
2022-03-17 19:41:59 +0800

    more routes

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
 
@@ -18,6 +18,22 @@ async def get(id: int) -> Optional[dict]:
         return summary
     return None
 
+
 async def get_all() -> List:
     summaries = await TextSummary.all().values()
     return summaries
+
+
+async def delete(id: int) -> int:
+    summary = await TextSummary.filter(id=id).first().delete()
+    return summary
+
+
+async def put(id: int, payload: SummaryPayloadSchema) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).update(
+        url=payload.url, summary=payload.summary
+    )
+    if summary:
+        updated_summary = await TextSummary.filter(id=id).first().values()
+        return updated_summary
+    return None
